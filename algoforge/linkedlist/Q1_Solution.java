60006331983e97921b08c01b8a29cd3706173db0
Ketan Kauntia <ketankauntia26@gmail.com>
2025-05-24 21:07:21 +0530

    LL: vod 2 done. Started with questions.

---
+++ algoforge/linkedlist/Q1_Solution.java
@@ -0,0 +1,45 @@
+package algoforge.linkedlist;
+
+//  GFG questions have this node structure.
+// struct Node {
+//   int data;
+//   struct Node *next;
+//   Node(int x) {
+//     data = x;
+//     next = NULL;
+//   }
+// };
+
+// Function to find the data of kth node from the end of a linked list.
+public class Q1_Solution {
+  public:
+    int getKthFromLast(Node *head, int k) {
+        // Your code here
+        Node* temp = head;
+        int len =1;
+        while(temp->next!=NULL){ // trying to count size
+            len++;
+            temp = temp->next;
+        }
+        
+        if(k>len){    // If k is more than the number of nodes, then the output should be -1
+            return -1;
+        }
+        
+        // (count - k) +1/next node.
+        // so we go to count-k th node and then read value of the next node.
+        temp = head;
+        int destinationNode = len-k+1 ;
+        
+        int count = 1;
+        while(count < destinationNode){
+            temp = temp->next;
+            count++;
+        }
+        
+        //next reached to 1 node before.
+        //reading data of next node and returing it.
+        int val = temp->data;
+        return val;
+    }
+};
