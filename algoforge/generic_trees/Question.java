e5a03ac362f165496833cb08070513ceaf4526bd
Ketan Kauntia <ketankauntia26@gmail.com>
2025-06-19 23:46:33 +0530

    add: 19-6-25 Stack Intro

---
+++ algoforge/generic_trees/Question.java
@@ -0,0 +1,28 @@
+package algoforge.generic_trees;
+
+// linearize a generic tree
+
+import java.util.ArrayList;
+import java.util.Stack;
+class Node {
+    int data;
+    ArrayList<Node> children;
+
+    public Node(){}
+
+    public Node(int data){
+        this.data = data;
+        this.children = new ArrayList<>();
+    }
+}
+
+public class Question {
+
+    public void linearize(Node n){
+            
+        while()
+    }
+    public static void main(String args[]){
+        
+    }
+}
