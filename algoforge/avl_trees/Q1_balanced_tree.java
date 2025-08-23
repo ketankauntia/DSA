014797c5b0eb91389ca7362e03a665625c683d05
Ketan Kauntia <ketankauntia26@gmail.com>
2025-08-23 10:00:00 +0000

    add: 23-8-25 BT vod10

---
+++ algoforge/avl_trees/Q1_balanced_tree.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+
+public class Q1_balanced_tree {
+
+    public int dfsHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftHeight =dfsHeight(root.left);
+        if(leftHeight == -1) return -1;
+
+        int rightHeight = dfsHeight(root.right);
+        if(rightHeight == -1) return -1;
+
+        if(Math.abs(leftHeight - rightHeight)>1) return -1;
+
+        return Math.max(leftHeight,rightHeight) +1;
+    }
+    
+    public boolean isBalanced(TreeNode root){
+        return dfsHeight(root)!=-1;
+    }
+}
